1af6ee09455f7aa2d75f55ce4416c2206fdeb731
Aidul Azmi. S <aidulazmi@gmail.com>
2021-06-28 20:44:21 +0700

    Skj

--- application/views/user/tamu/Tamu.php
+++ application/views/user/tamu/Tamu.php
@@ -300,10 +300,7 @@ RIGHT SIDEBAR TOGGLE SECTION
 							<div class="container">
 								<div class="carousel-caption">
 									<h3>Fathiya Poster</h3>
-									<a href="#" class="shop-button btn">
-										Shop Now
-									</a>
-
+									
 								</div>
 							</div>
 						</div>
@@ -354,14 +351,11 @@ RIGHT SIDEBAR TOGGLE SECTION
 					<div class="align-photo-6-inf-cols row">
 						
 						<div class="photo-6-inf-right col-lg-6">
-								<h3 class="hny-title text-left">Kartu Nama <span>30% Discount</span></h3>
-								<p>Silahkan Cetak Kartu Nama Anda</p>
-								<a href="#" class="read-more btn">
-										Shop Now
-								</a>
+								<h3 class="hny-title text-left">Undangan <span>30% Discount</span></h3>
+								<p>Silahkan Cetak Undangan Anda</p>
 						</div>
 						<div class="photo-6-inf-left col-lg-6">
-								<img src="<?=base_url();?>/asset/jualan/assets/images/1.jpg" class="img-fluid" alt="">
+								<img src="<?=base_url();?>/asset/loginu/images/kj.jpg" class="img-fluid" alt="">
 						</div>
 					</div>
 				 </div>
@@ -369,42 +363,25 @@ RIGHT SIDEBAR TOGGLE SECTION
 	 </section>
    <!-- //specification-6-->
      
-<section class="w3l-video-6">
-	<!-- /video-6-->
-	<div class="video-66-info">
-		<div class="container-fluid">
-			<div class="video-grids-info row">
-				<div class="video-gd-right col-lg-8">
-					<div class="video-inner text-center">
-								<!--popup-->
-									<a class="play-button btn popup-with-zoom-anim" href="#small-dialog">
-											<span class="fa fa-play" aria-hidden="true"></span>
-									</a>
-									<div id="small-dialog" class="mfp-hide">
-										<div class="search-top notify-popup">
-												<iframe src="https://player.vimeo.com/video/246139491" frameborder="0"
-												allow="autoplay; fullscreen" allowfullscreen></iframe>
-										</div>
-									</div>
-									<!--//popup-->
-					      </div>
-				  </div>
-				<div class="video-gd-left col-lg-4 p-lg-5 p-4">
-					<div class="p-xl-4 p-0 video-wrap">
-						<h3 class="hny-title text-left">Silahkan Cetak Undangan Pernikahan Anda <span>30% Discount</span>
-						</h3>
-						<p>Dibuat Oleh Para Desain grafis Profesional</p>
-						<a href="" class="read-more btn">
-							Shop Now
-						</a>
+<section class="w3l-content-w-photo-6">
+	<!-- /specification-6-->
+	  <div class="content-photo-6-mian py-5">
+			 <div class="container py-lg-5">
+					<div class="align-photo-6-inf-cols row">
+						
+						<div class="photo-6-inf-right col-lg-6">
+								<h3 class="hny-title text-left">Kartu Nama <span>30% Discount</span></h3>
+								<p>Silahkan Cetak Kartu Nama Anda</p>
+						</div>
+						<div class="photo-6-inf-left col-lg-6">
+								<img src="<?=base_url();?>/asset/loginu/images/gs.png" class="img-fluid" alt="">
+						</div>
 					</div>
-				</div>
+				 </div>
+			 </div>
+	 </section>
+   <!-- //specification-6-->
 
-			</div>
-		</div>
-	</div>
-</section>
-<!-- //video-6-->
 <section class="w3l-ecommerce-main">
 	<!-- /products-->
 	<div class="ecom-contenthny py-5">
@@ -422,7 +399,7 @@ RIGHT SIDEBAR TOGGLE SECTION
 				<div class="col-lg-3 col-6 product-incfhny mt-4">
 					<div class="product-grid2 transmitv">
 						<div class="product-image2">
-							<a href="ecommerce-single.html">
+							<a href="<?php echo site_url('C_user/detail/'.$u->id_barang);?>">
 								<img class="pic-1 img-fluid" src='<?=base_url()?>uploads/barang/<?=$u->foto1;?>'>
 								<img class="pic-2 img-fluid" src='<?=base_url()?>uploads/barang/<?=$u->foto2;?>'>
 							</a>
